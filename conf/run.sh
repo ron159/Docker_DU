5eeb5b3656f4aeb2dcfafc6f0ce9e0810f69862d
ron159 <zlronron159@gmail.com>
2019-05-26 20:36:25 +0800

    Update run.sh

--- conf/run.sh
+++ conf/run.sh
@@ -2,6 +2,5 @@
 
 do
   /usr/bin/aria2c --conf-path="/root/dl/conf/aria2.conf" -D \
-  && /usr/sbin/nginx -c /root/dl/conf/nginx.conf \
-  && tail -f /var/log/nginx/error.log
+  && nginx -g "daemon off;"
 done
