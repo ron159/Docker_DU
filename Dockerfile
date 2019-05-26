5a571ab000c8b5b41a33ed1b6d3ab7f3314c00f9
ron159 <zlronron159@gmail.com>
2019-05-26 15:32:22 +0800

    提交最新的配置文件以供使用

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,8 @@ FROM ubuntu:18.04
 
 LABEL AUTHOR=ron<zlronron159@gmail.com>
 
-WORKDIR /root/downloader
-ADD conf /root/downloader/conf
+WORKDIR /root/dl
+ADD conf /root/dl/conf
 
 
 RUN buildDeps='curl wget vim aria2 unzip nginx' \
@@ -13,8 +13,8 @@ RUN buildDeps='curl wget vim aria2 unzip nginx' \
     #&& apt-get purge -y --auto-remove $buildDeps \
     && mkdir /root/downloader/Dwonlad
     #AriaNg
-RUN mkdir /root/downloader/ariang \
-    && cd /root/downloader/ariang \
+RUN mkdir /root/dl/ariang \
+    && cd /root/dl/ariang \
     && wget https://github.com/mayswind/AriaNg/releases/download/1.1.0/AriaNg-1.1.0.zip \
     && unzip AriaNg-1.1.0.zip \
     && rm -rf AriaNg-1.1.0.zip \
--- conf/autoupload.sh
+++ conf/autoupload.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 path=$3 #取原始路径，我的环境下如果是单文件则为/data/demo.png,如果是文件夹则该值为文件夹内某个文件比如/data/a/b/c/d.jpg
 downloadpath='/Download'
- 
- 
+url='https://api.telegram.org/bot726919827:AAGg6D-pMg8P77pZrkBClPva4tz7Paatxaw/sendMessage?' 
 if [ "$2" = "0" ]
 	then
 		exit 0
 fi
 while true; do  #提取下载文件根路径，如把/data/a/b/c/d.jpg变成/data/a
 	filepath=$path
-	path=${path%/*}; 
+	path=${path%/*};
 	if [ "$path" = "$downloadpath" ] && [ $2 -eq 1 ]  #如果下载的是单个文件
 		then
-		rclone move "$filepath" gdteam:/     
+		curl -d "chat_id=@marktwo_post&text=["${filepath##*/}"]下载完成，正在执行上传..." "$url"
+		rclone move "$filepath" gd:/   
+		curl -d "chat_id=@marktwo_post&text=["${filepath##*/}"]上传完成！" "$url"  
 		exit 0
 	elif [ "$path" = "$downloadpath" ]   #文件夹
 		then							
-		while [[ "`ls -A "$filepath/"`" != "" ]]; do			
-		rclone move "$filepath"/ gdteam:/"${filepath##*/}"/ --delete-empty-src-dirs				
+		while [[ "`ls -A "$filepath/"`" != "" ]]; do
+		curl -d "chat_id=@marktwo_post&text=["${filepath##*/}"]下载完成，正在执行上传..." "$url"		
+		rclone move "$filepath"/ gd:/"${filepath##*/}"/ --delete-empty-src-dirs	
+		curl -d "chat_id=@marktwo_post&text=["${filepath##*/}"]上传完成！" "$url"			
 		done
 		rm -rf "$filepath/"	
 	      	exit 0
