a749a4016567082d60e4bc0b2fdb8ef383c54404
ron159 <zlronron159@gmail.com>
2019-05-26 18:55:49 +0800

    Update run.sh

--- conf/run.sh
+++ conf/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while true; do
-  /usr/bin/aria2c --conf-path="/root/dl/conf/aria2.conf" -D 
-  ##&& /usr/sbin/nginx -c /etc/nginx/nginx.conf
+do
+  /usr/bin/aria2c --conf-path="/root/dl/conf/aria2.conf" -D \
+  && service nginx start
 done
