0e8e6b221b405023a342e5a539bfc25a9b958463
ron159 <zlronron159@gmail.com>
2019-05-26 20:31:05 +0800

    Update run.sh

--- conf/run.sh
+++ conf/run.sh
@@ -2,5 +2,6 @@
 
 do
   /usr/bin/aria2c --conf-path="/root/dl/conf/aria2.conf" -D \
-  && /usr/sbin/nginx -c /root/dl/conf/nginx.conf
+  && /usr/sbin/nginx -c /root/dl/conf/nginx.conf \
+  && tail -f /var/log/nginx/error.log
 done
